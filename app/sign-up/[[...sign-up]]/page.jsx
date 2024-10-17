3a413dbd82f2d851ed7d38a021df2dbd055b6aa9
kelvin-oo <olamilekan.obisesan1@gmail.com>
2024-10-17 16:40:00 +0100

    Feat: File upload to aws s3 working in production.

--- app/sign-up/[[...sign-up]]/page.jsx
+++ app/sign-up/[[...sign-up]]/page.jsx
@@ -2,7 +2,7 @@ import { SignUp } from "@clerk/nextjs";
 
 export default function page() {
   return (
-   <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
+   <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 bg-black">
      <SignUp />
    </div>
   )
